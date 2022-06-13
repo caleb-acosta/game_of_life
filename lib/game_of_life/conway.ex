4a141cdc391233ce51004a68ac6c79d9d43778ab
Caleb Acosta <caleb.acosta.j@gmail.com>
2022-06-13 12:15:55 -0500

    Game of Life

---
+++ lib/game_of_life/conway.ex
@@ -0,0 +1,111 @@
+defmodule Sum do
+  defstruct value: 0
+
+  def value(%Sum{value: value}), do: value
+
+  def add(%Sum{value: value} = sum, x) do
+    %Sum{sum | value: value + x}
+  end
+
+  defimpl Collectable do
+    def into(original) do
+      {
+        original,
+        fn
+          sum, {:cont, x} -> Sum.add(sum, x)
+          sum, :done -> sum
+          _, :halt -> :ok
+        end
+      }
+    end
+  end
+end
+
+defmodule Conway.Grid do
+  defstruct data: nil
+
+  def new(size, prob) when is_integer(size) and size > 0 do
+    %Conway.Grid{
+      data:
+        new_data(size, fn _, _ ->
+          if :rand.uniform(10) - 1 < prob do
+            1
+          else
+            0
+          end
+        end)
+    }
+  end
+
+  def new(size) when is_integer(size) and size > 0 do
+    %Conway.Grid{
+      data: new_data(size, fn _, _ -> :rand.uniform(2) - 1 end)
+    }
+  end
+
+  def new(data) when is_list(data) do
+    %Conway.Grid{data: list_to_data(data)}
+  end
+
+  def size(%Conway.Grid{data: data}), do: tuple_size(data)
+
+  def cell_status(grid, x, y) do
+    grid.data
+    |> elem(y)
+    |> elem(x)
+  end
+
+  def next(grid) do
+    %Conway.Grid{grid | data: new_data(size(grid), &next_cell_status(grid, &1, &2))}
+  end
+
+  defp new_data(size, producer_fun) do
+    for y <- 0..(size - 1) do
+      for x <- 0..(size - 1) do
+        producer_fun.(x, y)
+      end
+    end
+    |> list_to_data
+  end
+
+  defp list_to_data(data) do
+    data
+    |> Enum.map(&List.to_tuple/1)
+    |> List.to_tuple()
+  end
+
+  def next_cell_status(grid, x, y) do
+    case {cell_status(grid, x, y), alive_neighbours(grid, x, y)} do
+      {1, 2} -> 1
+      {1, 3} -> 1
+      {0, 3} -> 1
+      {_, _} -> 0
+    end
+  end
+
+  defp alive_neighbours(grid, cell_x, cell_y) do
+    for x <- (cell_x - 1)..(cell_x + 1),
+        y <- (cell_y - 1)..(cell_y + 1),
+        x in 0..(size(grid) - 1) and
+          y in 0..(size(grid) - 1) and
+          (x != cell_x or y != cell_y) and
+          cell_status(grid, x, y) == 1,
+        into: %Sum{} do
+      1
+    end
+    |> Sum.value()
+  end
+end
+
+defmodule Conway.LiveGame do
+  def print(grid) do
+    for y <- 0..(Conway.Grid.size(grid) - 1) do
+      for x <- 0..(Conway.Grid.size(grid) - 1) do
+        case Conway.Grid.cell_status(grid, x, y) do
+          0 -> "cell"
+          1 -> "active-cell"
+        end
+      end
+    end
+  end
+end
---
+++ lib/game_of_life/live/game.ex
@@ -0,0 +1,74 @@
+defmodule GameOfLifeWeb.GameLive do
+  use Phoenix.LiveView
+  @topic "game"
+
+  def mount(_params, _session, socket) do
+    GameOfLifeWeb.Endpoint.subscribe(@topic)
+
+    {
+      :ok,
+      assign(
+        socket,
+        grid_size: 20,
+        grid_data: nil,
+        game_state: :stopped,
+        gen_prob: 5
+      )
+    }
+  end
+
+  def render(assigns), do: GameOfLifeWeb.PageView.render("game.html", assigns)
+
+  def handle_event("set-size", %{"size" => size}, socket) do
+    size = String.to_integer(size)
+    grid = for _ <- 1..size, do: 0 |> List.duplicate(size)
+    grid_data = Conway.Grid.new(grid)
+
+    GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update-grid", %{
+      grid_data: grid_data,
+      grid_size: size
+    })
+
+    {:noreply, assign(socket, grid_data: grid_data, grid_size: size)}
+  end
+
+  def handle_event("randomize", _values, socket) do
+    grid_data = Conway.Grid.new(socket.assigns.grid_size, socket.assigns.gen_prob)
+    GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update-grid", %{grid_data: grid_data})
+    {:noreply, assign(socket, grid_data: grid_data)}
+  end
+
+  def handle_event("go", _values, socket) do
+    GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update-grid", %{game_state: :playing})
+    Process.send_after(self(), :play, 500)
+    {:noreply, assign(socket, game_state: :playing)}
+  end
+
+  def handle_event("stop", _values, socket) do
+    GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update-grid", %{game_state: :stopped})
+    {:noreply, assign(socket, game_state: :stopped)}
+  end
+
+  def handle_event("chg-prob", %{"_target" => ["slider"], "slider" => prob}, socket) do
+    prob = String.to_integer(prob)
+    GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update-grid", %{gen_prob: prob})
+    {:noreply, assign(socket, gen_prob: prob)}
+  end
+
+  def handle_info(:play, socket) do
+    if socket.assigns.game_state == :playing do
+      grid_data = Conway.Grid.next(socket.assigns.grid_data)
+
+      GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update-grid", %{grid_data: grid_data})
+
+      Process.send_after(self(), :play, 1_000)
+      {:noreply, assign(socket, grid_data: grid_data)}
+    else
+      {:noreply, assign(socket, game_state: :stopped)}
+    end
+  end
+
+  def handle_info(info, socket) do
+    {:noreply, assign(socket, info.payload)}
+  end
+end
